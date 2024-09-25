e964b7c9de814f3d26d8cae0553d98bd0c036617
Tarek <Tarek@DESKTOP-S6Q0D90>
2024-09-25 16:56:24 +0300

    Add project files.

---
+++ Ruaad_Task_ Cruad/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ruaad_Task__Cruad.Models;
+
+namespace Ruaad_Task__Cruad.Controllers
+{
+    public class ProductController : Controller
+    {
+         
+        public IActionResult Index()
+        {
+            var products = new ApplicationDbContext();
+            var result = products.Products.ToList();
+            return View(result);
+        }
+        public IActionResult Details(int id) { 
+            var products = new ApplicationDbContext();
+            var result = products.Products.FirstOrDefault(p => p.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return View(result);
+        }
+        public IActionResult Edit(int id)
+        {
+            var products = new ApplicationDbContext();
+            var result = products.Products.FirstOrDefault(p => p.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product newProduct)
+        {
+            var products = new ApplicationDbContext();
+            var result = products.Products.FirstOrDefault(p => p.Id == newProduct.Id);
+            if (result != null)
+            {
+                result.Name = newProduct.Name;
+                result.Description = newProduct.Description;
+                result.Price = newProduct.Price;
+                result.ImageUrl = newProduct.ImageUrl;
+
+                products.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(newProduct);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var products = new ApplicationDbContext();
+            var result = products.Products.FirstOrDefault(p => p.Id == id);
+            if (result != null)
+            {
+                products.Products.Remove(result);
+                products.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Ruaad_Task_ Cruad/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ruaad_Task__Cruad.Models
+{
+	public class ApplicationDbContext:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=DESKTOP-S6Q0D90\\TAREKELABSY;Database=InStockDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true");
+		}
+		public DbSet<Product> Products { get; set; }
+	}
+}
